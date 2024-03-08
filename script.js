3816585e592a3c3435e0cbf01c0229fb339c738d
Ayaan Ahmed <ayaan8114iaaa@gmail.com>
2024-03-08 08:57:56 +0500

    Shop

---
+++ script.js
@@ -0,0 +1,53 @@
+function A() {
+    document.getElementById("A").style.display = "flex";
+    document.getElementById("B").style.display = "none";
+    document.getElementById("C").style.display = "none";
+    document.getElementById("D").style.display = "none";
+    document.getElementById("E").style.display = "none";
+    document.getElementById("F").style.display = "none";
+}
+
+function B() {
+    document.getElementById("B").style.display = "flex";
+    document.getElementById("A").style.display = "none";
+    document.getElementById("C").style.display = "none";
+    document.getElementById("D").style.display = "none";
+    document.getElementById("E").style.display = "none";
+    document.getElementById("F").style.display = "none";
+}
+
+function C() {
+    document.getElementById("C").style.display = "flex";
+    document.getElementById("B").style.display = "none";
+    document.getElementById("A").style.display = "none";
+    document.getElementById("D").style.display = "none";
+    document.getElementById("E").style.display = "none";
+    document.getElementById("F").style.display = "none";
+}
+
+function D() {
+    document.getElementById("D").style.display = "flex";
+    document.getElementById("B").style.display = "none";
+    document.getElementById("C").style.display = "none";
+    document.getElementById("A").style.display = "none";
+    document.getElementById("E").style.display = "none";
+    document.getElementById("F").style.display = "none";
+}
+
+function E() {
+    document.getElementById("E").style.display = "flex";
+    document.getElementById("B").style.display = "none";
+    document.getElementById("C").style.display = "none";
+    document.getElementById("D").style.display = "none";
+    document.getElementById("A").style.display = "none";
+    document.getElementById("F").style.display = "none";
+}
+
+function F() {
+    document.getElementById("F").style.display = "flex";
+    document.getElementById("B").style.display = "none";
+    document.getElementById("C").style.display = "none";
+    document.getElementById("D").style.display = "none";
+    document.getElementById("E").style.display = "none";
+    document.getElementById("A").style.display = "none";
+}
